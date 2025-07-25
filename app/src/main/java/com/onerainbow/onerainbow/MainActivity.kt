53753e2901f3b0c39eb5c790f2fe33212326328d
summer <2992203079@qq.com>
2025-07-25 20:02:35 +0800

    :sparkles: 新增开屏动画

--- app/src/main/java/com/onerainbow/onerainbow/MainActivity.kt
+++ app/src/main/java/com/onerainbow/onerainbow/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.onerainbow.onerainbow
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import com.onerainbow.lib.base.BaseActivity
 import com.onerainbow.lib.route.RoutePath
 import com.onerainbow.onerainbow.databinding.ActivityMainBinding
 import com.therouter.TheRouter
 import com.therouter.router.Route
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Route(path = RoutePath.MAIN)
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -20,6 +25,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun initEvent() {
-        TheRouter.build(RoutePath.HOME).navigation(this)
+        playSplashAnimation()
     }
+
+    private fun playSplashAnimation() {
+        lifecycleScope.launch {
+            delay(2000)
+            TheRouter.build(RoutePath.HOME).navigation()
+            // 设置启动淡入淡出动画
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+
+        }
+
+    }
+
 }
